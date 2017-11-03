8e482d15839334c5fa1bb077e662670d04bc7fdc
Ricardo Amaral <master@ricardoamaral.net>
2017-11-03 15:48:47 +0000

    Allow document/window variables to be mangled

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,4 +1,4 @@
-(function () {
+(function (document, window) {
     // Private auxiliary functions
 
     function initTwinklingStars() {
@@ -78,4 +78,4 @@
     window.onorientationchange = () => {
         document.body.scrollTop = 0;
     };
-}());
+}(document, window));
